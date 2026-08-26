a105add33bebb792bd5735f272044f6c8c477ea3
Daniella <daniella.leon@tecsup.edu.pe>
2026-08-26 00:42:08 -0500

    Proyecto inicial con menú del carrito

---
+++ app/src/main/java/com/Leon/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.Leon.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
